78315124d5f7d2938b58a8d818499a6583788287
魂魇桀 <67376760+hunyanjie@users.noreply.github.com>
2025-09-08 18:53:04 +1000

    修改品牌故事

--- js/index.js
+++ js/index.js
@@ -1,1 +1,15 @@
 console.log('Happy developing ✨')
+
+window.addEventListener('scroll', function() {
+    const whyChooseUs = document.getElementById('why-choose-us');
+    const triggerPoint = document.getElementById('hero-1618').offsetTop + 300; // Adjust the offset as needed
+    if (window.scrollY <= triggerPoint) {
+        whyChooseUs.classList.remove('show');
+    }
+    if (window.scrollY > triggerPoint) {
+        whyChooseUs.classList.add('show');
+    }
+    if (window.scrollY > (document.getElementById('hero-1618').offsetHeight - 200)) {
+        whyChooseUs.classList.remove('show');
+    }
+});
